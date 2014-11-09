6bbaff8591706a1f5f4d2bec98507e6ebe333ec8
John Setting <jsetting32@gmail.com>
2014-11-09 01:54:33 -0800

    Initial Commit

---
+++ JSCalendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSCalendar
+//
+//  Created by John Setting on 11/9/14.
+//  Copyright (c) 2014 John Setting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSCalendar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JSCalendar
+//
+//  Created by John Setting on 11/9/14.
+//  Copyright (c) 2014 John Setting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
